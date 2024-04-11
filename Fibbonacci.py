617a2b0dbeb1a499cf26af752f37a30a0f03fa40
Lucas-BZan <lucasbrisch@gmail.com>
2024-04-11 20:59:43 -0300

    Fibbonaacci funcionando

--- Fibbonacci.py
+++ Fibbonacci.py
@@ -1,9 +1,9 @@
-def main():
-    final = int(input("Digite um número para ver a sequência de Fibonacci: "))
-    cont = 1
-    fibbonacci = 0
-    while cont  < final + 1:
-        fibbonacci += cont
-        cont += 1
-    print(f"O número de Fibonacci é {fibbonacci}")
-main()
+def fibonacci(n):
+    sequence = [0, 1]
+    while sequence[-1] < n :
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence
+
+num = int(input("Digite um número: "))
+fib_sequence = fibonacci(num)
+print(f"A sequência de Fibonacci até o número {num} é: {fib_sequence[len(fib_sequence)-2]}")
