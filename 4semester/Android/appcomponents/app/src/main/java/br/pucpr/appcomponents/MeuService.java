c426ed05e7accaf125b86850d8bb599852bbddb0
Lucas Brisch <lucasbrisch@gmail.com>
2025-08-25 19:43:55 -0300

    ANDROID - aprendendo a usar service e log

--- 4semester/Android/appcomponents/app/src/main/java/br/pucpr/appcomponents/MeuService.java
+++ 4semester/Android/appcomponents/app/src/main/java/br/pucpr/appcomponents/MeuService.java
@@ -3,9 +3,42 @@ package br.pucpr.appcomponents;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MeuService extends Service {
     public MeuService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        // Recupera os valores enviados pela Activity
+        double primeiro = intent.getDoubleExtra("primeiro", 0.0);
+        double segundo = intent.getDoubleExtra("segundo", 0.0);
+
+        // Executa o cálculo como soma repetida
+        double resultado = 0;
+        for (int i = 0; i < segundo; i++) {
+            resultado += primeiro;
+        }
+
+        Log.d("AppComponentes", "Valor A: " + primeiro);
+        Log.d("AppComponentes", "Valor B: " + segundo);
+        Log.d("AppComponentes", "Resposta: " + resultado);
+
+
+        // Envia uma Intent broadcast avisando que o cálculo terminou
+        Intent broadcastIntent = new Intent("CALCULO_FINALIZADO");
+        sendBroadcast(broadcastIntent);
+
+        // Encerra o serviço após a execução
+        stopSelf();
+
+        // Indica que o serviço não será recriado automaticamente
+        return START_NOT_STICKY;
+
+
+
     }
 
     @Override
--- 4semester/Android/appcomponents/app/src/main/java/br/pucpr/appcomponents/MinhaActivity.java
+++ 4semester/Android/appcomponents/app/src/main/java/br/pucpr/appcomponents/MinhaActivity.java
@@ -1,18 +1,13 @@
 package br.pucpr.appcomponents;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import android.widget.EditText;
-import android.widget.Toast;
-import android.widget.Button;
 
 public class MinhaActivity extends AppCompatActivity {
 
@@ -21,32 +16,33 @@ public class MinhaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_minha);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        EditText edtPrimeiro = findViewById(R.id.editTextText3);
-        EditText edtSegundo = findViewById(R.id.editTextText4);
+        Button btnIniciar = findViewById(R.id.btnIniciar);
+        Button btnConsultar = findViewById(R.id.btnConsultar);
 
-        Button btnIniciar = findViewById(R.id.button3);
-        Button btnconsultar = findViewById(R.id.button4);
+        EditText edtPrimeiro = findViewById(R.id.edtPrimeiro);
+        EditText edtSegundo = findViewById(R.id.edtSegundo);
 
         btnIniciar.setOnClickListener(v -> {
+
+
+
             double primeiro = Double.parseDouble(edtPrimeiro.getText().toString());
             double segundo = Double.parseDouble(edtSegundo.getText().toString());
 
             Intent intent = new Intent(this, MeuService.class);
-            intent.putExtra("prieiro", primeiro);
+            intent.putExtra("primeiro", primeiro);
             intent.putExtra("segundo", segundo);
 
-            Toast.makeText(this, "clicou", Toast.LENGTH_LONG).show();
+            startService(intent);
+
+            Toast.makeText(this, "Clicou", Toast.LENGTH_LONG).show();
         });
 
-        btnconsultar.setOnClickListener ( v -> {
+        btnConsultar.setOnClickListener(v -> {
             Intent intent = new Intent(this, SegundaActivity.class);
             startActivity(intent);
         });
+
     }
 }
