7836fa7f1a56e41575b3397c011acdb13cb5c96d
Lucas Brisch <lucasbrisch@gmail.com>
2025-09-04 20:53:17 -0300

    JAVA BACKEND - exception

--- 4semester/web-development-backend/aulawebservice2025-main/src/main/java/br/pucpr/projetowebservice/controller/UsuarioController.java
+++ 4semester/web-development-backend/aulawebservice2025-main/src/main/java/br/pucpr/projetowebservice/controller/UsuarioController.java
@@ -1,25 +1,35 @@
 package br.pucpr.projetowebservice.controller;
 
 import br.pucpr.projetowebservice.dto.UsuarioDTO;
+import br.pucpr.projetowebservice.exception.BusinessException;
+import br.pucpr.projetowebservice.exception.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/usuario")
+@Tag(name = "Usuário", description = "APIs de gerenciamento de usuários")
 public class UsuarioController {
 
     private List<UsuarioDTO> usuarios = new ArrayList<>();
 
     @PostMapping
+    @Operation(summary = "Salva um usuário", description = "Salva um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário Salvo com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Os dados do usuário estão incorretos."),
+    })
     public ResponseEntity<UsuarioDTO> save(@Valid @RequestBody UsuarioDTO usuarioDTO) {
         usuarioDTO.setId(1);
         usuarios.add(usuarioDTO);
@@ -27,18 +37,24 @@ public class UsuarioController {
     }
 
     @GetMapping
-    @Operation(summary = "obter usuario por ID", description = "retorna a lista de usuarios")
+    @Operation(summary = "Obter a lista de usuários", description = "Retorna a lista de usuários")
     @ApiResponses(value = {
-            @ApiResponse (responseCode = "200", description = "Recuperado com sucesso"),
+            @ApiResponse(responseCode = "200", description = "Recuperado com sucesso"),
     })
     public List<UsuarioDTO> findAll() {
         return usuarios;
     }
 
     @PutMapping("/{id}")
-    public UsuarioDTO update(@PathVariable("id") Integer id, @RequestBody UsuarioDTO usuarioDTO) {
+    public ResponseEntity<UsuarioDTO> update(@PathVariable("id") Integer id, @RequestBody UsuarioDTO usuarioDTO)
+            throws BusinessException {
+        if (id == null || usuarioDTO.getId() == null) {
+            throw new BusinessException("ID_REQUIRED","O ID é necessário");
+        }
+
         usuarioDTO.setId(id);
-        return usuarioDTO;
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioDTO);
     }
 
     @DeleteMapping("/{id}")
